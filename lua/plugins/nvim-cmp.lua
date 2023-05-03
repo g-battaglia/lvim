b4c42e095a998afa5c3844538cf960e50b7fcef4
g-battaglia-h-farm <giacomo.battaglia@h-farm.com>
2023-05-03 18:37:56 +0200

    Updates

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -6,17 +6,20 @@ return {
     event = "InsertEnter",
     opts = function(_, opts)
       local cmp = require("cmp")
+      opts.completion = {
+        completeopt = "menu,menuone,noinsert,noselect",
+      }
       opts.mapping = cmp.mapping.preset.insert({
-        ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-        ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+        ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete(),
         ["<C-e>"] = cmp.mapping.abort(),
         -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
         -- The behavior fixes indentation errors.
-        ["<C-a>"] = cmp.mapping.confirm({
-          behavior = cmp.ConfirmBehavior.Replace,
+        ["<CR>"] = cmp.mapping.confirm({
+          -- behavior = cmp.ConfirmBehavior.Replace,
           select = false,
         }),
       })
