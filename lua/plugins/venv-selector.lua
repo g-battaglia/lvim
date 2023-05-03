d188df8765eeef330198cea967a2ff567db06094
g-battaglia <battaglia.giacomo@icloud.com>
2023-05-03 15:58:15 +0200

    Updates

---
+++ lua/plugins/venv-selector.lua
@@ -0,0 +1,11 @@
+return {
+  "linux-cultist/venv-selector.nvim",
+  dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim" },
+  config = true,
+  event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
+  keys = { {
+    "<leader>ce", "<cmd>:VenvSelect<cr>", "Change Python virtual environment"
+    -- key mapping for directly retrieve from cache. You may set autocmd if you prefer the no hand approach
+    --"<leader>ce", "<cmd>:VenvSelectCached<cr>"
+  } }
+}
--- lua/plugins/vim-plug.lua
+++ lua/plugins/vim-plug.lua
@@ -1,8 +1,4 @@
 return {
   { "mtdl9/vim-log-highlighting" },
   { "evanleck/vim-svelte" },
-  -- Fixme
-
-  "AckslD/swenv.nvim",
-  "stevearc/dressing.nvim",
 }
