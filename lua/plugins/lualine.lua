e3601f3421ad867dbde25e192b15d7584c2aa911
g-battaglia-h-farm <giacomo.battaglia@jakala.com>
2024-05-29 16:57:54 +0200

    Updates

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,10 +1,12 @@
 return {
   "nvim-lualine/lualine.nvim",
   opts = function(_, opts)
+    --[[
     table.insert(opts.sections.lualine_x, 2, {
       "copilot",
       -- default is true.
       show_running = true,
     })
+    --]]
   end,
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,20 +5,8 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    dependencies = {
-      {
-        "HiPhish/nvim-ts-rainbow2",
-      },
-    },
     opts = {
       -- nvim-ts-rainbow2 configuration
-      rainbow = {
-        enable = true,
-        -- list of languages you want to disable the plugin for
-        disable = {},
-        -- Which query to use for finding delimiters
-        query = "rainbow-parens",
-      },
       ensure_installed = {
         "bash",
         "vimdoc",
