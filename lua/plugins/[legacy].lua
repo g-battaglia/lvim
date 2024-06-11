1e612da5ae5abf207ec1909293890f5db4bb16d2
g-battaglia-h-farm <giacomo.battaglia@jakala.com>
2024-06-11 17:21:15 +0200

    Clean up

--- lua/plugins/[legacy].lua
+++ lua/plugins/[legacy].lua
@@ -1,10 +1,7 @@
 return {
   { "mtdl9/vim-log-highlighting" },
-  { "evanleck/vim-svelte" },
   { "github/copilot.vim" },
   { "prettier/vim-prettier" },
-  { "g-battaglia/lualine-copilot" },
-  { "mbbill/undotree" },
   { "mechatroner/rainbow_csv" },
   { "mustache/vim-mustache-handlebars" },
 }
