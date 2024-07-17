fbddb198b479bf30c0ccc56de31e3563144503ee
g-battaglia <battaglia.giacomo@icloud.com>
2024-07-17 15:38:16 +0200

    Updates

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -2,7 +2,7 @@ return {
   "folke/which-key.nvim",
   event = "VeryLazy",
   opts = {
-    defaults = {
+    spec = {
       ["<leader>t"] = { name = "+nvterm" },
       ["<leader>p"] = { name = "+prettier" },
       ["<leader>n"] = { name = "+neorg" },
