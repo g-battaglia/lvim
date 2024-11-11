8d4e9b52057cf8269fb8d2a614888594f2198ac9
g-battaglia-h-farm <giacomo.battaglia@jakala.com>
2024-11-11 15:31:16 +0100

    Updates

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,12 @@
+return {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  opts = {
+    styles = {
+      terminal = {
+        height = 0.3, -- Set the terminal height as a fraction of the window
+      },
+    },
+  },
+}
