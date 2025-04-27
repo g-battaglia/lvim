d2fee9a1441caac1e4a8cd242937fb4eeda25cad
g-battaglia <battaglia.giacomo@icloud.com>
2025-04-27 23:42:22 +0200

    Disabled Dashboard

--- lua/plugins/[disabled].lua
+++ lua/plugins/[disabled].lua
@@ -3,5 +3,6 @@ return {
   { "catppuccin/nvim",                         enabled = false },
   { "folke/flash.nvim",                        enabled = false },
   { "RRethy/vim-illuminate",                   enabled = false },
-  { "nvim-treesitter/nvim-treesitter-context", enabled = false }
+  { "nvim-treesitter/nvim-treesitter-context", enabled = false },
+  {"nvimdev/dashboard-nvim",                   enabled = false }
 }
