5467ae1fad6e547f914bd15565cf901b3a7620dd
g-battaglia-h-farm <giacomo.battaglia@jakala.com>
2025-09-11 18:06:44 +0200

    toggle light mode comand

--- init.lua
+++ init.lua
@@ -4,3 +4,16 @@ require("config.lazy")
 -- LSP Servers
 require("config.servers")
 
+-- Temporary
+local function toggle_light_mode()
+  if vim.o.background == "light" then
+    vim.o.background = "dark"
+    vim.cmd.colorscheme("tokyonight-moon")
+  else
+    vim.o.background = "light"
+    vim.cmd.colorscheme("everforest")
+  end
+end
+
+vim.api.nvim_create_user_command("ToggleLightMode", toggle_light_mode, {})
+vim.keymap.set("n", "<leader>tl", toggle_light_mode, { desc = "Toggle Light Mode" })
