a22ef68b9be2d0ee37fa12fa8e13700bd14866a1
g-battaglia <battaglia.giacomo@icloud.com>
2026-01-15 20:55:11 +0100

    fix

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,6 +18,13 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins", branch = "main" },
+
+    -- Lazy extras
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.lang.vue" },
+
     -- import/override with your plugins
     { import = "plugins" },
   },
